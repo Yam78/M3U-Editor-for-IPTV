0ab2f6f230eb73a8831cef2f5e627f374441578d
Yam78 <sami.p.vaisanen@gmail.com>
2019-07-10 15:53:42 +0300

    Initial upload
    Very basic functionality...

---
+++ M3U Editor/M3U Editor/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace M3U_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importM3UToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportM3UToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.CBGroups = new System.Windows.Forms.ComboBox();
+            this.lbChannels = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(306, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importM3UToolStripMenuItem,
+            this.exportM3UToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importM3UToolStripMenuItem
+            // 
+            this.importM3UToolStripMenuItem.Enabled = false;
+            this.importM3UToolStripMenuItem.Name = "importM3UToolStripMenuItem";
+            this.importM3UToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importM3UToolStripMenuItem.Text = "Import M3U";
+            this.importM3UToolStripMenuItem.Click += new System.EventHandler(this.ImportM3UToolStripMenuItem_Click);
+            // 
+            // exportM3UToolStripMenuItem
+            // 
+            this.exportM3UToolStripMenuItem.Enabled = false;
+            this.exportM3UToolStripMenuItem.Name = "exportM3UToolStripMenuItem";
+            this.exportM3UToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportM3UToolStripMenuItem.Text = "Export M3U";
+            this.exportM3UToolStripMenuItem.Click += new System.EventHandler(this.ExportM3UToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(12, 27);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(280, 23);
+            this.BtnBrowse.TabIndex = 1;
+            this.BtnBrowse.Text = "Browse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CBGroups
+            // 
+            this.CBGroups.FormattingEnabled = true;
+            this.CBGroups.Location = new System.Drawing.Point(12, 56);
+            this.CBGroups.Name = "CBGroups";
+            this.CBGroups.Size = new System.Drawing.Size(280, 21);
+            this.CBGroups.TabIndex = 5;
+            this.CBGroups.SelectedIndexChanged += new System.EventHandler(this.CBGroups_SelectedIndexChanged);
+            // 
+            // lbChannels
+            // 
+            this.lbChannels.FormattingEnabled = true;
+            this.lbChannels.Location = new System.Drawing.Point(12, 83);
+            this.lbChannels.Name = "lbChannels";
+            this.lbChannels.Size = new System.Drawing.Size(280, 355);
+            this.lbChannels.TabIndex = 6;
+            this.lbChannels.SelectedIndexChanged += new System.EventHandler(this.lbChannels_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 450);
+            this.Controls.Add(this.lbChannels);
+            this.Controls.Add(this.CBGroups);
+            this.Controls.Add(this.BtnBrowse);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importM3UToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportM3UToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.ComboBox CBGroups;
+        private System.Windows.Forms.ListBox lbChannels;
+    }
+}
+
---
+++ M3U Editor/M3U Editor/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace M3U_Editor
+{
+    public partial class Form1 : Form
+    {
+        List<ChannelData> Channels = new List<ChannelData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.Filter = "M3U Playlist (*.m3u)|*.m3u|All files (*.*)|*.*";
+            ofd.ShowDialog();
+            string curFile = ofd.FileName;
+
+            Channels.Clear();
+            string[] lines = System.IO.File.ReadAllLines(curFile);
+
+            for (int i = 0; i < lines.Count() - 1; i++)
+            {
+                // Detect Start, ignore first line
+                if (lines[i].StartsWith("#EXTM3U"))
+                {
+                    // Do nothing
+                }
+                if (lines[i].StartsWith("#EXTINF:-1"))
+                {
+                    ProcessLine(lines[i], lines[i + 1]);
+                    i = i + 1;
+                }
+            }
+
+            // Fill groups
+            CBGroups.Items.AddRange((from chn in Channels select chn.GroupTitle).Distinct<String>().ToArray<Object>());
+        }
+
+        private void ProcessLine(string line1, string line2)
+        {
+            string processingLine = line1;
+            ChannelData data = new ChannelData();
+
+            // ID
+            string[] separators = { "tvg-", "group-" };
+            string[] subLines = processingLine.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            data.ID = subLines[1].Substring(subLines[1].IndexOf("\"")).Replace("\"", "").Trim();
+            data.Name = subLines[2].Substring(subLines[2].IndexOf("\"")).Replace("\"", "").Trim();
+            data.LogoURL = subLines[3].Substring(subLines[3].IndexOf("\"")).Replace("\"", "").Trim();
+            
+            string[] groupSplitted = subLines[4].Substring(subLines[4].IndexOf("\"")).Trim().Split(',');
+            data.GroupTitle = groupSplitted[0].Replace("\"","");
+            data.VisibleName = groupSplitted[1];
+            data.StreamUrl = line2;
+            Channels.Add(data);
+        }
+
+        private void CBGroups_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbChannels.Items.Clear();
+            lbChannels.Items.AddRange((from chn in Channels where chn.GroupTitle == CBGroups.Text  select chn).ToArray<Object>());
+        }
+
+        private void lbChannels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChannelData selectedChannel = (ChannelData)lbChannels.SelectedItem;
+
+            Process p=null ;
+            if (IsProcessOpen("vlc",ref p))
+            {
+                p.Kill();
+            }
+                p = new Process();
+                p.StartInfo.FileName = @"C:\Program Files\VideoLAN\VLC\vlc.exe";
+                p.StartInfo.Arguments = selectedChannel.StreamUrl;
+                p.Start();
+        }
+
+        public bool IsProcessOpen(string name, ref Process proc)
+        {
+            //here we're going to get a list of all running processes on
+            //the computer
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                //now we're going to see if any of the running processes
+                //match the currently running processes. Be sure to not
+                //add the .exe to the name you provide, i.e: NOTEPAD,
+                //not NOTEPAD.EXE or false is always returned even if
+                //notepad is running.
+                //Remember, if you have the process running more than once, 
+                //say IE open 4 times the loop thr way it is now will close all 4,
+                //if you want it to just close the first one it finds
+                //then add a return; after the Kill
+                if (clsProcess.ProcessName.Contains(name))
+                {
+                    //if the process is found to be running then we
+                    //return a true
+                    proc = clsProcess;
+                    return true;
+                }
+            }
+            //otherwise we return a false
+            return false;
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void ImportM3UToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExportM3UToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+    public class ChannelData
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string LogoURL { get; set; }
+        public string GroupTitle { get; set; }
+        public string VisibleName { get; set; }
+        public string StreamUrl { get; set; }
+
+        public ChannelData()
+        {
+        }
+
+        public override string ToString()
+        {
+            return VisibleName ;
+        }
+    }
+}
